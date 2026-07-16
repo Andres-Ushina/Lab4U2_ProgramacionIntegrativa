c0b82597c2d52f9e2ce9d7133968b5f282ac938e
Andres Ushina <ajushina@espe.edu,ec>
2026-07-16 10:39:15 -0500

    Actualización para descargar localmente

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
-
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js'
-  }
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        short_name: "APP PWA",
+        name: "Mi aplicación PWA",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        theme_color: "black",
+        background_color: "white",
+      },
+    }),
+  ],
+});
